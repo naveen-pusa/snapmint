63fa7bc6ba09bd88ebdcc23c5655af4cc7e0e17c
Naveen-pusa <naveenpusa2000@gmail.com>
2026-05-29 12:08:36 +0530

    cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,7 +10,7 @@ function Cart({ cartItems, onUpdateQuantity, onRemoveItem, onClearCart }) {
 
         /* ── EMPTY STATE ── */
         <div className="text-center py-4 px-3">
-          <div style={{ fontSize: '48px' }}>🛒</div>
+          <div  style={{ fontSize: '48px' }}>🛒</div>
           <h6  className="fw-bold mt-2">Your cart is empty</h6>
           <p className="text-muted small">Add some products!</p>
         </div>
