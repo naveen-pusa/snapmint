cfdbb189c228b30fddd3a94b7bf6ee4c53f8c5f5
Naveen-pusa <naveenpusa2000@gmail.com>
2026-05-30 14:32:08 +0530

    reomved comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Home from './pages/Home'
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+ 
+import Authmodal from './pages/Authmodal'        
+import Home from './pages/Home'                  
+import SingleProduct from './pages/SingleProduct' 
+ import ProtectedRoute from './components/ProtectedRoute' 
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-  <Home/>
-    </>
+    <Routes>
+
+         {/* PROTECTED — only accessible after login */}
+      <Route path="/" element={
+        <ProtectedRoute><Home /></ProtectedRoute>
+      } />
+
+
+      {/* PUBLIC — /auth is landing page (signup/login) */}
+      <Route path="/auth" element={<Authmodal />} />
+
+   
+      <Route path="/product/:id" element={
+        <ProtectedRoute><SingleProduct /></ProtectedRoute>
+      } />
+
+      {/* <Route path="/cart" element={
+        <ProtectedRoute><CartPage /></ProtectedRoute>
+      } /> */}
+
+      {/* Any unknown URL → /auth */}
+      <Route path="*" element={<Navigate to="/auth" replace />} />
+
+    </Routes>
   )
 }
 
