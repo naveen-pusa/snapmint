d3452659cbac100c2b3c646626e2ccdeb12b0c71
Naveen-pusa <naveenpusa2000@gmail.com>
2026-06-05 13:06:53 +0530

    update productedRouter

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 
 function ProtectedRoute({ children }) {
-  const user = (() => {
+   const user = (() => {
     try {
       const saved = localStorage.getItem('snapmint_loggedIn')
       return saved ? JSON.parse(saved) : null
