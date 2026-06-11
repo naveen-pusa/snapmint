cbb17e64b2cfb39098fa634dfa14e0445e65b817
Naveen-pusa <naveenpusa2000@gmail.com>
2026-06-11 17:03:29 +0530

    update

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -28,8 +28,8 @@ function SingleProduct() {
 
   const handleAddToCart = () => {
    const cartItems = JSON.parse(localStorage.getItem('snapmint_cart') || '[]')
-    const existingIndex = cartItems.findIndex(item => item.id === product.id)
-    if  (existingIndex !== -1) {
+   const existingIndex = cartItems.findIndex(item => item.id === product.id)
+    if (existingIndex !== -1) {
         for (let i = 0; i < quantity; i++) {
         cartItems[existingIndex].qty += 1
       }
