84aa8fc0301c44fbac569e663b4b0bd3d78dc81b
Naveen-pusa <naveenpusa2000@gmail.com>
2026-06-13 10:51:42 +0530

    update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Carousel() {
 return (
     <> 
-     <div id="carouselExampleCaptions" className="carousel slide">
+    <div id="carouselExampleCaptions" className="carousel slide">
     <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
