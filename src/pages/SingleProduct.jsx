393ce0d8da63bd50871fd1289a3efe19ad393f8e
Naveen-pusa <naveenpusa2000@gmail.com>
2026-06-29 10:59:31 +0530

    update

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -30,7 +30,7 @@ function SingleProduct() {
    const cartItems = JSON.parse(localStorage.getItem('snapmint_cart') || '[]')
    const existingIndex = cartItems.findIndex(item => item.id === product.id)
      if (existingIndex !== -1) {
-        for (let i = 0; i < quantity; i++) {
+       for (let i = 0; i < quantity; i++) {
       cartItems[existingIndex].qty += 1
       }
     } else {
